55d74c61cd01bd73e9c30dfd481e1231e4e6aadd
diyajennath <jennathdiya821@gmail.com>
2024-09-24 18:25:49 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import Navbar from './components/Navbar';
+import Banner from './components/Banner';
+import Content from './components/Content';
+import Technology from './components/Technology';
+import FAQs from './components/Faq';
+import HowWeWork from './components/HowWeWork';
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Banner />
+      <Content />
+      <HowWeWork />
+      <Technology />
+      <FAQs />
+    
+     
     </div>
   );
 }
